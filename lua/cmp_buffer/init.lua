e92ff2276854998f764b9f25fa5c9dbc01172479
hrsh7th <hrsh7th@gmail.com>
2021-08-25 11:37:06 +0900

    Add get_keyword_pattern API

--- lua/cmp_buffer/init.lua
+++ lua/cmp_buffer/init.lua
@@ -15,6 +15,15 @@ source.new = function()
   return self
 end
 
+source.get_keyword_pattern = function(_, params)
+  params.option = vim.tbl_deep_extend('keep', params.option, defaults)
+  vim.validate({
+    keyword_pattern = { params.option.keyword_pattern, 'string', '`opts.keyword_pattern` must be `string`' },
+    get_bufnrs = { params.option.get_bufnrs, 'function', '`opts.get_bufnrs` must be `function`' },
+  })
+  return params.option.keyword_pattern
+end
+
 source.complete = function(self, params, callback)
   params.option = vim.tbl_deep_extend('keep', params.option, defaults)
   vim.validate({
